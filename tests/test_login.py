11b2b546cd7e7f0abb398e6de7614afb65370f6c
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2025-02-13 21:20:03 +0300

    Add allure

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,10 @@
 import pytest
+import allure
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 
 
+@allure.title("Название теста")
 def test_login_failure(page):
     login_page = LoginPage(page)
     login_page.navigate()
@@ -10,6 +12,9 @@ def test_login_failure(page):
     assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
 
+@allure.epic("Web interface")
+@allure.feature("Доступ к системе")
+@allure.story("Авторизаци")
 def test_login_success(page):
     login_page = LoginPage(page)
     dashboard_page = DashboardPage(page)
@@ -24,7 +29,7 @@ def test_login_success(page):
     ('user', 'user'),
     ('admin', 'admin')
 ])
-def test_login_success(page, username, password):
+def test_login_success0(page, username, password):
     login_page = LoginPage(page)
     dashboard_page = DashboardPage(page)
 
@@ -35,6 +40,7 @@ def test_login_success(page, username, password):
 
 
 #Используем фикстуры для упрощения тестов.
+@allure.description("Описание теста")
 def test_login_failure_with_fixture(login_page):
     login_page.navigate()
     login_page.login('user', 'password')
@@ -42,11 +48,12 @@ def test_login_failure_with_fixture(login_page):
     assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
 
+@allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.parametrize('username, password', [
     ('user', 'user'),
     ('admin', 'admin')
 ])
-def test_login_success(login_page, dashboard_page, username, password):
+def test_login_success1(login_page, dashboard_page, username, password):
     login_page.navigate()
     login_page.login(username, password)
     dashboard_page.assert_welcome_message(f"Welcome {username}")
